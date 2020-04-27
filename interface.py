a50a62e38cf3091b7279802321ad9196216a9b45
Bryan <bryangregory@live.com>
2020-04-26 21:45:38 -0400

    Added the option to view data for a month

--- interface.py
+++ interface.py
@@ -63,18 +63,47 @@ def main():
                         print("Today's entry for {} has been updated".format(cat.name.capitalize()))
                     conn.commit()
             elif choose == 'v':
-                date = input("[t]oday or [o]ther day: ").lower()
+                date = input("[t]oday, [o]ther day, [m]onth: ").lower()
+
+                one_flag = True
+                one_string = 'one'
+                day_string = 'and day = '
+
                 if date == 't':
                     date = datetime.datetime.today()
                 elif date == 'o':
                     raw_date = input("Enter date as YYYY-MM-DD: ")
                     date = datetime.date(year=int(raw_date[0:4]), month=int(raw_date[5:7].strip("0")), day=int(raw_date[8:10]))
+                elif date == 'm':
+                    one_flag = False
+                    raw_date = input("Enter date as YYYY-MM: ")
+                    date = datetime.date(year=int(raw_date[0:4]), month=int(raw_date[5:7].strip("0")), day=1)
 
                 cat_choice = input("View data for which category? ").lower()
                 cat_type = read_database(conn, "type", "Categories", "WHERE name = \"{}\"".format(cat_choice), "string", "one")
-                entry_data = read_database(conn, "data", "Entries", "WHERE category_name = \"{}\" and year = {} and month = {} and day = {}".format(cat_choice, date.year, date.month, date.day), cat_type, "one")
 
-                if entry_data is not None: print("{}'s entry for {} is {}".format(date.strftime('%Y-%m-%d'), cat_choice.capitalize(), entry_data))
+                if not one_flag:
+                    one_string = 'all'
+                    day_string = ''
+                else:
+                    day_string += str(date.day)
+                entry_data = read_database(conn, "data", "Entries",
+                                               "WHERE category_name = \"{}\" and year = {} and month = {} {}".format(
+                                                   cat_choice, date.year, date.month, day_string), cat_type, one_string)
+
+                if entry_data is not None:
+                    if one_flag:
+                        print(
+                            "{}'s entry for {} is {}".format(date.strftime('%Y-%m-%d'), cat_choice.capitalize(), entry_data))
+                    else:
+                        sum = 0
+                        print("{}'s entries for {}".format(date.strftime('%Y-%m'), cat_choice.capitalize()))
+                        for entry in entry_data:
+                            if entry[0].isnumeric():
+                                sum +=int (entry[0])
+                            print("{}".format(entry[0]))
+                        if sum > 0:
+                            print("Total: {}".format(sum))
 
             elif choose == 'q':
                 print('Goodbye')
