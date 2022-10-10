52539fd8fe728568a9395e4294563ba78b10dc10
Bryan <bryangrigorie@gmail.com>
2022-10-09 21:20:04 -0400

    finish migration

--- controller.py
+++ controller.py
@@ -83,36 +83,42 @@ class Controller:
                     self.view.val_var.set(entry.data)
                     self.view.des_var.set(self.get_description(self.view.cat_var.get().lower()))
 
-            # Pull reminder info for queried category  # TODO migrate
+            # Pull reminder info for queried category 
             try:
-                if self.db.exists(self.db.conn, "Reminders", "category_name", f"\"{self.view.cat_var.get().lower()}\""):
+                reminder = self.session.query(model.Reminder)\
+                    .filter(
+                        model.Reminder.category_name_from_any == self.view.cat_var.get().lower()
+                    ).first()
+                # if self.db.exists(self.db.conn, "Reminders", "category_name", f"\"{self.view.cat_var.get().lower()}\""):  # ! Deprecated
+                if reminder:
                     self.view.remind_var.set(1)
 
             except Exception as e:
                 self.view.remind_var.set(0)
         elif caption == "Update":
             try:
-                # If there is input  # TODO migrate
+                # If there is input
                 if self.view.cat_var.get() != '' and self.view.val_var.get() != '':
-                    if self.db.read_database(self.db.conn, "data", "Entries",
-                                            "WHERE category_name = \"{}\" and year = {} and month = {} and day = {}".format(
-                                                    self.view.cat_var.get().lower(), self.view.date.year, self.view.date.month, self.view.date.day), "string", "one") is None:
-                         self.db.write_database(self.db.conn, "insert", "Entries", "category_name, year, month, day, data",
-                                              "\"{}\", {}, {}, {}, \"{}\"".format(self.view.cat_var.get().lower(), self.view.date.year, self.view.date.month, self.view.date.day,
-                                                                                  self.view.val_var.get().lower()))
-                    else:
-                        self.db.write_database(self.db.conn, "update", "Entries", "data = {}".format(self.view.val_var.get().lower()),
-                                          "WHERE category_name = \"{}\" and year = {} and month = {} and day = {}".format(
-                                              self.view.cat_var.get().lower(), self.view.date.year, self.view.date.month, self.view.date.day))
+                    # if self.db.read_database(self.db.conn, "data", "Entries",  # ! Deprecated
+                    #                         "WHERE category_name = \"{}\" and year = {} and month = {} and day = {}".format(
+                    #                                 self.view.cat_var.get().lower(), self.view.date.year, self.view.date.month, self.view.date.day), "string", "one") is None:
+                    #      self.db.write_database(self.db.conn, "insert", "Entries", "category_name, year, month, day, data",
+                    #                           "\"{}\", {}, {}, {}, \"{}\"".format(self.view.cat_var.get().lower(), self.view.date.year, self.view.date.month, self.view.date.day,
+                    #                                                               self.view.val_var.get().lower()))
+                    # else:
+                    #     self.db.write_database(self.db.conn, "update", "Entries", "data = {}".format(self.view.val_var.get().lower()),
+                    #                       "WHERE category_name = \"{}\" and year = {} and month = {} and day = {}".format(
+                    #                           self.view.cat_var.get().lower(), self.view.date.year, self.view.date.month, self.view.date.day))
+                    self.set_cat_value(self.view.cat_var.get().lower(), self.view.date, self.view.val_var.get().lower())
 
-                    # Inserts category into database if it is novel  # TODO migrate
-                    cat_type = "string"
-                    if self.view.cat_var.get().isnumeric():
-                        cat_type = "float"
-                    self.db.write_database(self.db.conn, "insert","Categories","name, type, description", f"\"{self.view.cat_var.get().lower()}\", \"{cat_type}\", \"\"")
+                    # Inserts category into database if it is novel 
+                    # cat_type = "string"  # ! Deprecated
+                    # if self.view.cat_var.get().isnumeric():
+                    #     cat_type = "float"
+                    # self.db.write_database(self.db.conn, "insert","Categories","name, type, description", f"\"{self.view.cat_var.get().lower()}\", \"{cat_type}\", \"\"")
 
-                    # Updates entry  # TODO migrate
-                    self.db.conn.commit()
+                    # Updates entry  
+                    # self.db.conn.commit()  # ! Deprecated
                     self.view.popup_window("Success", f"Entry has been updated to {self.view.val_var.get()}")
                 else: raise Exception("Missing input in fields")
             except Exception as e: self.view.popup_window("Error", e)
@@ -133,92 +139,111 @@ class Controller:
                                    )
         elif caption=="Sum (month)":
             if self.view.cat_var.get() != '':  # if there is valid input
-                try:  # TODO migrate
-                    data_set = self.db.read_database(self.db.conn, "data", "Entries",
-                                          f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year} and month = {self.view.date.month}", "int", "all")
+                try:
+                    # data_set = self.db.read_database(self.db.conn, "data", "Entries",  # ! Deprecated
+                    #                       f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year} and month = {self.view.date.month}", "int", "all")
+                    data_set = self.session.query(model.Entry).filter_by(year=self.view.date.year, month=self.view.date.month)\
+                        .filter(
+                            model.Entry.category_name_from_any==self.view.cat_var.get().lower()
+                        ).options(load_only('data')).all()
 
                     sum = 0
-                    for data in data_set:
-                        sum+=int(data[0])
+                    for entry in data_set:
+                        sum+=int(getattr(entry, 'data', 0))
 
                     self.view.popup_window("Result", f"Sum of \"{self.view.cat_var.get()}\" values for {self.view.date.month}-{self.view.date.year}:\n\n{sum}")
                 except Exception as e: self.view.popup_window("Error", e)
         elif caption=="Sum (year)":
             if self.view.cat_var.get() != '':  # if there is valid input
-                try:  # TODO migrate
-                    data_set = self.db.read_database(self.db.conn, "data", "Entries",
-                                          f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year}", "int", "all")
-
+                try:
+                    # data_set = self.db.read_database(self.db.conn, "data", "Entries",  # ! Deprecated
+                    #                       f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year}", "int", "all")
+                    data_set = self.session.query(model.Entry).filter_by(year=self.view.date.year)\
+                        .filter(
+                            model.Entry.category_name_From_any==self.view.cat_var.get().lower()
+                        ).options(load_only('data')).all()
                     sum = 0
-                    for data in data_set:
-                        sum+=int(data[0])
+                    for entry in data_set:
+                        sum+=int(getattr(entry, 'data', 0))
 
                     self.view.popup_window("Result", f"Sum of \"{self.view.cat_var.get()}\" values for {self.view.date.year}:\n\n{sum}")
                 except Exception as e: self.view.popup_window("Error", e)
         elif caption=="Average (month)":
             if self.view.cat_var.get() != '':
-                try:  # TODO migrate
-                    data_set = self.db.read_database(self.db.conn, "data", "Entries",
-                                                     f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year} and month = {self.view.date.month}",
-                                                     "int", "all")
-
+                try:
+                    # data_set = self.db.read_database(self.db.conn, "data", "Entries",  # ! Deprecated
+                    #                                  f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year} and month = {self.view.date.month}",
+                    #                                  "int", "all")
+                    data_set = self.session.query(model.Entry).filter_by(year=self.view.date.year, month=self.view.date.month)\
+                        .filter(
+                            model.Entry.category_name_from_any==self.view.cat_var.get().lower()
+                        ).options(load_only('data')).all()
                     sum = 0
-                    count = 0
-                    for data in data_set:
-                        sum += int(data[0])
-                        count += 1
+                    for entry in data_set:
+                        sum += int(getattr(entry, 'data', 0))
 
-                    average = round(sum / count, 2)
+                    average = round(sum / len(data_set), 2)
 
                     self.view.popup_window("Result",
-                                       f"Average of \"{self.view.cat_var.get()}\" values for {self.view.date.month}-{self.view.date.year}:\n\n{average} (given {count} entries)")
+                                       f"Average of \"{self.view.cat_var.get()}\" values for {self.view.date.month}-{self.view.date.year}:\n\n{average} (given {len(data_set)} entries)")
                 except Exception as e:
                     self.view.popup_window("Error", e)
         elif caption=="Average (year)":
             if self.view.cat_var.get() != '':
-                try:  # TODO migrate
-                    data_set = self.db.read_database(self.db.conn, "data", "Entries",
-                                                     f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year}",
-                                                     "int", "all")
+                try:
+                    # data_set = self.db.read_database(self.db.conn, "data", "Entries",  # ! Deprecated
+                    #                                  f"WHERE category_name = \"{self.view.cat_var.get().lower()}\" and year = {self.view.date.year}",
+                    #                                  "int", "all")
+                    data_set = self.session.query(model.Entry).filter_by(year=self.view.date.year)\
+                        .filter(
+                            model.Entry.category_name_from_any==self.view.cat_var.get().lower()
+                        ).options(load_only('data')).all()
 
                     sum = 0
-                    count = 0
-                    for data in data_set:
-                        sum += int(data[0])
-                        count += 1
+                    for entry in data_set:
+                        sum += int(getattr(entry, 'data', 0))
 
-                    average = round(sum / count, 2)
+                    average = round(sum / len(data_set), 2)
 
                     self.view.popup_window("Result",
-                                           f"Average of \"{self.view.cat_var.get()}\" values for {self.view.date.year}:\n\n{average} (given {count} entries)")
+                                           f"Average of \"{self.view.cat_var.get()}\" values for {self.view.date.year}:\n\n{average} (given {len(data_set)} entries)")
                 except Exception as e:
                     self.view.popup_window("Error", e)
-        elif caption == "Toggle reminder for category":  # TODO migrate
+        elif caption == "Toggle reminder for category":
             boolean = self.view.remind_var.get()
-            if boolean == 1 and self.view.cat_var.get().lower() != '':
-                self.db.write_database(self.db.conn, "insert", "Reminders", "category_name", f"\"{self.view.cat_var.get().lower()}\"")
-                self.db.conn.commit()
-            elif boolean == 0 and self.view.cat_var.get().lower() != '':
-                self.db.write_database(self.db.conn, "delete", "Reminders", "category_name",
-                                       f"\"{self.view.cat_var.get().lower()}\"")
-                self.db.conn.commit()
+            category = self.session.query(model.Category).filter(
+                or_(model.Category.name == self.view.cat_var.get().lower(), model.Category.id == self.view.cat_var.get().lower())
+            ).first()
+            if boolean == 1 and category:
+                # self.db.write_database(self.db.conn, "insert", "Reminders", "category_name", f"\"{self.view.cat_var.get().lower()}\"")  # ! Deprecated
+                # self.db.conn.commit()
+                self.session.add(model.Reminder(category_id=category.id))
+                self.session.commit()
+            elif boolean == 0 and category:
+                # self.db.write_database(self.db.conn, "delete", "Reminders", "category_name",  # ! Deprecated
+                #                        f"\"{self.view.cat_var.get().lower()}\"")
+                # self.db.conn.commit()
+                self.session.delete(category.reminder)
+                self.session.commit()
             elif boolean == 1 and self.view.cat_var.get().lower() == '':
                 self.view.remind_var.set(0)
 
-        elif caption == "List of categories":  # TODO migrate
+        elif caption == "List of categories":
             message = ''
-            cats = self.db.read_database(self.db.conn, "name", "Categories", None, "string", "all")
+            # cats = self.db.read_database(self.db.conn, "name", "Categories", None, "string", "all")  # ! Deprecated
+            cats = self.session.query(model.Category).options(load_only('name')).all()
             for cat in cats:
-                message+=cat[0]+'\n'
+                message+=f'{cat.name}\n'
 
             self.view.popup_window("Categories", message)
-        elif caption == "Entries for this day":  # TODO migrate
+        elif caption == "Entries for this day":
             message = ''
-            entries = self.db.read_database(self.db.conn, "category_name, data", "Entries",
-                                            f"WHERE year = {self.view.date.year} and month = {self.view.date.month} and day = {self.view.date.day}",
-                                            "string", "all")
-            for name, data in entries:
-                message+=f"{name}: {data}\n"
+            # entries = self.db.read_database(self.db.conn, "category_name, data", "Entries",  # ! Deprecated
+            #                                 f"WHERE year = {self.view.date.year} and month = {self.view.date.month} and day = {self.view.date.day}",
+            #                                 "string", "all")
+            entries = self.session.query(model.Entry).filter_by(year=self.view.date.year, month=self.view.date.month, day=self.view.date.day).all()
+            for entry in entries:
+                message+=f"{entry.category.name}: {entry.data}\n"
 
             self.view.popup_window("Entries for this day", message)
 
@@ -232,6 +257,25 @@ class Controller:
         if entry:
             return entry.data
         return None
+
+    # Sets value for a category, date. Creates if not exists
+    def set_cat_value(self, cat_id_or_name, date, value):
+        category = self.session.query(model.Category).filter(or_(model.Category.name==cat_id_or_name, model.Category.id==cat_id_or_name)).first()
+        if not category:
+            # You wouldn't be passing an ID if the category didn't exist
+            category = model.Category(name=cat_id_or_name, type="float" if value.isnumeric() else "string")
+            self.session.add(category)
+        current_entry = self.session.query(model.Entry).filter_by(year=date.year, month=date.month, day=date.day)\
+            .filter(
+                model.Entry.category_name_from_any==cat_id_or_name
+            ).first()
+        if current_entry:
+            current_entry.data = value
+        else:
+            new_entry = model.Entry(year=date.year, month=date.month, day=date.day, category_id=category.id, value=value)
+            self.session.add(new_entry)
+        self.session.commit()
+
 
     # Checks db for set reminders
     def check_reminders(self):
@@ -269,7 +313,7 @@ class Controller:
     def set_description(self, desc, cat):
         # self.db.write_database(self.db.conn, "update", "Categories", f"description = \"{desc}\"", f"WHERE name = \"{cat}\"")
         # self.db.conn.commit()  # ! Deprecated
-        self.session.query(model.Category).filter_by(name=cat).update(
+        self.session.query(model.Category).filter(or_(model.Category.name==cat, model.Category.id==cat)).update(
             {'description': desc}
         )
         self.session.commit()
